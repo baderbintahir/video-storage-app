0c94502ff962af2aec4b4cc6d8d5b476f26569e0
Bader <muhammad.bader@dvsum.com>
2025-01-15 11:45:14 +0500

    mongoDB replaced with postgresql

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,19 +1,23 @@
 const User = require("../models/userModel");
 
 const getAllUsers = async () => {
-  return await User.find();
+  return await User.findAll();
 };
 
 const getUserById = async (id) => {
-  return await User.findById(id);
+  return await User.findByPk(id);
 };
 
 const updateUserById = async (id, userData) => {
-  return await User.findByIdAndUpdate(id, userData, { new: true });
+  const user = await User.findByPk(id);
+  if (!user) throw new Error('User not found');
+  return await user.update(userData);
 };
 
 const deleteUserById = async (id) => {
-  return await User.findByIdAndDelete(id);
+  const user = await User.findByPk(id);
+  if (!user) throw new Error('User not found');
+  await user.destroy();
 };
 
 module.exports = {
--- src/services/videoService.js
+++ src/services/videoService.js
@@ -2,35 +2,38 @@ const Video = require("../models/videoModel");
 
 // Get all videos
 const getAllVideos = async () => {
-  return await Video.find().sort({ createdAt: -1 });
+  return await Video.findAll();
 };
 
 // Get video by ID
 const getVideoById = async (id) => {
-  return await Video.findById(id);
+  return await Video.findByPk(id);
 };
 
 // Save video
 const saveVideo = async (videoData) => {
-  const video = new Video(videoData);
-  await video.save();
-  return video;
+  return await Video.create(videoData);
 };
 
 // Update video by ID
 const updateVideoById = async (id, videoData) => {
-  const video = await Video.findByIdAndUpdate(id, videoData, { new: true });
-  if (!video) throw new Error("Video not found");
-  return video;
+  const video = await Video.findByPk(id);
+  if (!video) throw new Error('Video not found');
+  return await video.update(videoData);
 };
 
 // Mark/unmark video as favorite
 const toggleFavorite = async (id) => {
-  const video = await Video.findById(id);
-  if (!video) throw new Error("Video not found");
-
-  const updatedVideo = await Video.findByIdAndUpdate(id, { favorite: !video.favorite }, { new: true });
-  return updatedVideo;
+  const video = await Video.findByPk(id);
+  if (!video) throw new Error('Video not found');
+  video.favorite = !video.favorite;
+  return await video.save();
 };
 
-module.exports = { getAllVideos, getVideoById, saveVideo, updateVideoById, toggleFavorite };
+module.exports = {
+  getAllVideos,
+  getVideoById,
+  saveVideo,
+  updateVideoById,
+  toggleFavorite,
+};
